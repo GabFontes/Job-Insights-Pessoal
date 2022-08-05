81ab7e670e1bab812b8852989f14fda1d563547a
gab <gabrielfontes321@gmail.com>
2022-08-05 15:25:37 -0300

    feat(src): implements test_counter function (requirement 10)

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    word_count = count_ocurrences("src/jobs.csv", "Python")
+    assert type(word_count) == int
+    assert word_count == 1639
+    word_count = count_ocurrences("src/jobs.csv", "python")
+    assert type(word_count) == int
+    assert word_count == 1639
+    word_count = count_ocurrences("src/jobs.csv", "zero")
+    assert type(word_count) == int
+    assert word_count == 14
