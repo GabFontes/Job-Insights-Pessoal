328fcd1c5cbb7a5291a4c07023a61a2b82008885
gab <gabrielfontes321@gmail.com>
2022-08-05 16:06:32 -0300

    excluding expected_brazilian_jobs_value folder

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,12 +1,9 @@
 from src.brazilian_jobs import read_brazilian_file
-from expected_brazilian_jobs_value import expected_values
 
 
 def test_brazilian_jobs():
     dict_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
-    mock = expected_values()
     assert type(dict_jobs) == list
-    assert dict_jobs == mock
     assert len(dict_jobs) == 15
     for job in dict_jobs:
         if 'title' in job and 'salary' in job and 'type' in job:
