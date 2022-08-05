735775cdb22486484ede981e36cf894c1ee6e185
gab <gabrielfontes321@gmail.com>
2022-08-05 16:02:14 -0300

    feat(src): implements test_brazilian_jobs (requirement 11)

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,14 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+from expected_brazilian_jobs_value import expected_values
 
 
 def test_brazilian_jobs():
-    pass
+    dict_jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    mock = expected_values()
+    assert type(dict_jobs) == list
+    assert dict_jobs == mock
+    assert len(dict_jobs) == 15
+    for job in dict_jobs:
+        if 'title' in job and 'salary' in job and 'type' in job:
+            expected_keys = True
+    assert expected_keys is True
